222d3c04ca364d3def2775caab91484b27747473
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-17 22:38:11 -0600

    Create FrozenRealmGenerator.java

---
+++ src/main/java/com/advancedworldgen/generator/FrozenRealmGenerator.java
@@ -0,0 +1,90 @@
+package com.advancedworldgen.generator;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.generator.BiomeProvider;
+import org.bukkit.generator.BlockPopulator;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.WorldInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FrozenRealmGenerator extends ChunkGenerator {
+
+    @Override
+    public void generateSurface(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {
+        long seed = worldInfo.getSeed();
+        NoiseUtil base = new NoiseUtil(seed);
+        NoiseUtil mount = new NoiseUtil(seed + 1000);
+        NoiseUtil detail = new NoiseUtil(seed + 2000);
+        NoiseUtil cave = new NoiseUtil(seed + 3000);
+        NoiseUtil cave2 = new NoiseUtil(seed + 4000);
+        NoiseUtil glacier = new NoiseUtil(seed + 7000);
+
+        for (int x = 0; x < 16; x++) {
+            for (int z = 0; z < 16; z++) {
+                int wx = chunkX * 16 + x;
+                int wz = chunkZ * 16 + z;
+
+                double baseH = base.octaveNoise(wx / 200.0, wz / 200.0, 6, 0.55, 2.1);
+                double mountH = mount.octaveNoise(wx / 140.0, wz / 140.0, 5, 0.65, 2.3);
+                double det = detail.octaveNoise(wx / 45.0, wz / 45.0, 3, 0.4, 2.0);
+                double glacierH = glacier.octaveNoise(wx / 100.0, wz / 100.0, 4, 0.5, 2.0);
+
+                double mf = Math.max(0, mountH);
+                mf = Math.pow(mf, 1.4);
+
+                // Glaciares y montañas heladas enormes
+                double heightD = 64 + baseH * 30 + mf * 140 + det * 8 + glacierH * 25;
+                int height = (int) Math.max(4, Math.min(319, heightD));
+
+                chunkData.setBlock(x, -64, z, Material.BEDROCK);
+                for (int y = -63; y <= -60; y++)
+                    if (random.nextInt(4 - (y + 63)) == 0)
+                        chunkData.setBlock(x, y, z, Material.BEDROCK);
+
+                for (int y = -59; y < height; y++) {
+                    double c1 = cave.octaveNoise(wx / 40.0, wz / 40.0 + y / 40.0, 3, 0.5, 2.0);
+                    double c2 = cave2.octaveNoise(wx / 40.0 + y / 40.0, wz / 40.0, 3, 0.5, 2.0);
+                    boolean isCave = y > -30 && y < height - 5 && (c1 * c1 + c2 * c2) < 0.08;
+
+                    if (!isCave) {
+                        if (y < -48) chunkData.setBlock(x, y, z, random.nextInt(20) == 0 ? Material.DEEPSLATE_DIAMOND_ORE : Material.DEEPSLATE);
+                        else if (y < 0) chunkData.setBlock(x, y, z, Material.STONE);
+                        else if (y < 80) chunkData.setBlock(x, y, z, random.nextInt(8) == 0 ? Material.PACKED_ICE : Material.STONE);
+                        else chunkData.setBlock(x, y, z, random.nextInt(5) == 0 ? Material.BLUE_ICE : Material.PACKED_ICE);
+                    }
+                }
+
+                for (int y = height - 3; y < height; y++)
+                    if (y >= -64) chunkData.setBlock(x, y, z, Material.SNOW_BLOCK);
+
+                if (height >= 0) chunkData.setBlock(x, height, z, Material.SNOW_BLOCK);
+
+                // Lagos de hielo
+                if (height < 62) {
+                    for (int y = height + 1; y <= 62; y++) chunkData.setBlock(x, y, z, Material.ICE);
+                    chunkData.setBlock(x, height, z, Material.SNOW_BLOCK);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void generateBedrock(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {}
+
+    @Override
+    public BiomeProvider getDefaultBiomeProvider(WorldInfo worldInfo) {
+        return new FrozenBiomeProvider(worldInfo.getSeed());
+    }
+
+    @Override
+    public List<BlockPopulator> getDefaultPopulators(World world) {
+        List<BlockPopulator> p = new ArrayList<>();
+        p.add(new FrozenTreePopulator());
+        p.add(new EpicCaveDecoratorPopulator());
+        return p;
+    }
+}
