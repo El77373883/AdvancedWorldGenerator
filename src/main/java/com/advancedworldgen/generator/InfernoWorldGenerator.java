c8e4773da3f7181225d930a7c0a64f394d29bb4c
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-17 22:37:23 -0600

    Create InfernoWorldGenerator.java

---
+++ src/main/java/com/advancedworldgen/generator/InfernoWorldGenerator.java
@@ -0,0 +1,95 @@
+package com.advancedworldgen.generator;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.generator.BiomeProvider;
+import org.bukkit.generator.BlockPopulator;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.WorldInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class InfernoWorldGenerator extends ChunkGenerator {
+
+    @Override
+    public void generateSurface(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {
+        long seed = worldInfo.getSeed();
+        NoiseUtil base = new NoiseUtil(seed);
+        NoiseUtil mount = new NoiseUtil(seed + 1000);
+        NoiseUtil detail = new NoiseUtil(seed + 2000);
+        NoiseUtil cave = new NoiseUtil(seed + 3000);
+        NoiseUtil cave2 = new NoiseUtil(seed + 4000);
+        NoiseUtil lava = new NoiseUtil(seed + 6000);
+
+        for (int x = 0; x < 16; x++) {
+            for (int z = 0; z < 16; z++) {
+                int wx = chunkX * 16 + x;
+                int wz = chunkZ * 16 + z;
+
+                double baseH = base.octaveNoise(wx / 160.0, wz / 160.0, 6, 0.6, 2.2);
+                double mountH = mount.octaveNoise(wx / 120.0, wz / 120.0, 5, 0.65, 2.3);
+                double det = detail.octaveNoise(wx / 40.0, wz / 40.0, 3, 0.4, 2.0);
+                double lavaH = lava.octaveNoise(wx / 80.0, wz / 80.0, 3, 0.5, 2.0);
+
+                double mf = Math.max(0, mountH);
+                mf = Math.pow(mf, 1.3);
+
+                // Montañas volcanicas enormes
+                double heightD = 64 + baseH * 40 + mf * 150 + det * 10 + lavaH * 15;
+                int height = (int) Math.max(4, Math.min(319, heightD));
+
+                chunkData.setBlock(x, -64, z, Material.BEDROCK);
+                for (int y = -63; y <= -60; y++)
+                    if (random.nextInt(4 - (y + 63)) == 0)
+                        chunkData.setBlock(x, y, z, Material.BEDROCK);
+
+                for (int y = -59; y < height; y++) {
+                    double c1 = cave.octaveNoise(wx / 40.0, wz / 40.0 + y / 40.0, 3, 0.5, 2.0);
+                    double c2 = cave2.octaveNoise(wx / 40.0 + y / 40.0, wz / 40.0, 3, 0.5, 2.0);
+                    boolean isCave = y > -30 && y < height - 5 && (c1 * c1 + c2 * c2) < 0.09;
+
+                    if (!isCave) {
+                        if (y < -48) chunkData.setBlock(x, y, z, random.nextInt(10) == 0 ? Material.DEEPSLATE_DIAMOND_ORE : Material.DEEPSLATE);
+                        else if (y < -20) chunkData.setBlock(x, y, z, random.nextInt(15) == 0 ? Material.BLACKSTONE : Material.DEEPSLATE);
+                        else if (y < 30) chunkData.setBlock(x, y, z, random.nextInt(20) == 0 ? Material.MAGMA_BLOCK : Material.BLACKSTONE);
+                        else chunkData.setBlock(x, y, z, random.nextInt(10) == 0 ? Material.BASALT : Material.BLACKSTONE);
+                    } else {
+                        // Cuevas de lava
+                        if (y < 20 && random.nextInt(8) == 0) chunkData.setBlock(x, y, z, Material.LAVA);
+                    }
+                }
+
+                for (int y = height - 2; y < height; y++)
+                    if (y >= -64) chunkData.setBlock(x, y, z, Material.BLACKSTONE);
+
+                if (height >= 0) {
+                    Material top = height > 150 ? Material.BASALT : height > 80 ? Material.BLACKSTONE : Material.MAGMA_BLOCK;
+                    chunkData.setBlock(x, height, z, top);
+                }
+
+                // Lagos de lava en vez de agua
+                if (height < 62) {
+                    for (int y = height + 1; y <= 62; y++) chunkData.setBlock(x, y, z, Material.LAVA);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void generateBedrock(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {}
+
+    @Override
+    public BiomeProvider getDefaultBiomeProvider(WorldInfo worldInfo) {
+        return new InfernoBiomeProvider(worldInfo.getSeed());
+    }
+
+    @Override
+    public List<BlockPopulator> getDefaultPopulators(World world) {
+        List<BlockPopulator> p = new ArrayList<>();
+        p.add(new InfernoTreePopulator());
+        p.add(new EpicCaveDecoratorPopulator());
+        return p;
+    }
+}
